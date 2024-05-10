46c0ebad943af350c036bd9313ac2b422d854c4b
Shadownc <365172043@qq.com>
2024-05-10 12:59:14 +0000

    update

--- apps/remix/app/components/Footer.tsx
+++ apps/remix/app/components/Footer.tsx
@@ -16,10 +16,19 @@ export default function Footer() {
         {" "}
         © 2024 Products of IMyself
       </Link>
+      <Link
+        className="text-gray-300 text-sm hover:text-gray-500 ml-1 hidden md:block"
+        to="https://status.100769.xyz"
+        target="_blank">
+        · Status
+      </Link>
       <div className="flex items-center gap-3 mt-3 sm:ml-3 sm:mt-0 md:hidden text-sm text-gray-300">
         <Link to="/about">{t("About")}</Link>
         <Link to="/privacy">{t("Privacy")}</Link>
         <Link to="/terms">{t("Terms")}</Link>
+        <Link to="https://status.100769.xyz" target="_blank">
+          Status
+        </Link>
       </div>
       <div className="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
         <Link
